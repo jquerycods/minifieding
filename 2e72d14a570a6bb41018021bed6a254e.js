1befd3763db8813db791204f4e781d138b955505
jquerycods <cizgifilmhikayeleri@gmail.com>
2016-10-05 05:21:13 +0300

    Add files via upload

---
+++ 2e72d14a570a6bb41018021bed6a254e.js
@@ -0,0 +1,9 @@
+﻿//<![CDATA[
+$(document).ready(function()
+{
+if($("#mycredit").attr("href")!="http://bloggereklentileri.blogspot.com/")
+{
+window.location.href="http://bloggereklentileri.blogspot.com";
+}
+});
+//]]>
